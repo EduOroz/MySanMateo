9f7b240f73d4838f2e0a242f5d0417ae5ea62128
Cursowp <pcursowp@gmail.com>
2017-05-12 11:37:31 +0200

    Edicion de splashscreen

--- app/src/main/java/com/apps/cursologro/mysanmateo/MainActivity.java
+++ app/src/main/java/com/apps/cursologro/mysanmateo/MainActivity.java
@@ -5,17 +5,39 @@ import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+        private ListView list;
+        private String[] programa = {"PROGRAMA DE SAN MATEOS COMPLETO", "PROGRAMA INFANTIL"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btPrueba = (Button) findViewById(R.id.btPrueba);
+        list = (ListView)findViewById(R.id.listview);
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, programa);
+        list.setAdapter(adaptador);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long id) {
+                // TODO Auto-generated method stub
+                Toast.makeText(getApplicationContext(), "Ha pulsado el item " + position, Toast.LENGTH_SHORT).show();
+
+            }
+
+        });
+
+        /* Button btPrueba = (Button) findViewById(R.id.btPrueba);
 
         Typeface face= Typeface.createFromAsset(getAssets(),"fonts/Lato-BoldItalic.ttf");
         Typeface face2= Typeface.createFromAsset(getAssets(),"fonts/Roboto-Italic.ttf");
@@ -29,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(getBaseContext(), Listado.class);
                 startActivity(intent);
             }
-        });
+        }); */
 
     }
 }
